11de890340432fedb58b3d999189c8c2bbb76e1c
Candido Regis <candidorbb@gmail.com>
2019-10-20 15:13:22 -0400

    db files added

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers (burger_name) 
+    VALUES ('X-burger');
+INSERT INTO burgers (burger_name) 
+    VALUES ('Double Cheese Turkey Burger');
+INSERT INTO burgers (burger_name) 
+    VALUES ('Hamburger');
+INSERT INTO burgers (burger_name, devoured) 
+    VALUES ('Vegan Burger', true);
+INSERT INTO burgers (burger_name, devoured) 
+    VALUES ('Hot Dog X-Burger', true);
+INSERT INTO burgers (burger_name, devoured) 
+    VALUES ('Fried Chicken Burger', true);
--- server.js
+++ server.js
@@ -10,13 +10,15 @@ var PORT = process.env.PORT || 3100;
 
 // MIDDLEWARE
 app.use(express.static(path.join(__dirname, "public/"), {maxAge: 3456700000})); 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
 app.use(express.json());
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-
 // ROUTES
-require("./controllers/burgers_controller.js")(app);
+var routes = require("./controllers/burgersController.js");
+app.use(routes);
 
 // STARTING THE SERVER
 app.listen(PORT, function() {
