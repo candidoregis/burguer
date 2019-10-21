2ff9201ec71e984a124a68b0e9394e2300a09557
Candido Regis <candidorbb@gmail.com>
2019-10-20 21:12:57 -0400

    server fix

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 // DEPENDENCIES
 var express = require("express");
-var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
 
 // EXPRESS CONFIG
 var app = express();
 var PORT = process.env.PORT || 3100;
 
 // MIDDLEWARE
-app.use(express.static(path.join(__dirname, "public/"), {maxAge: 3456700000})); 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(methodOverride('_method'));
-app.use(express.json());
+app.use(bodyParser.json());
+
+// HANDLEBARS
+var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
 // ROUTES
-var routes = require("./controllers/burgersController.js");
+var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 // STARTING THE SERVER
